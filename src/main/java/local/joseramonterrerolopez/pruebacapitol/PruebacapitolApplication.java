33cdcac695276e434075c1f1ad15883521622b1e
Jose Ramon Terrero Lopez <joseramonterrerolopez@gmail.com>
2024-09-04 18:53:01 +0200

    chore: initial setup of the Spring Boot framework
    - add initial dependencies in pom.xml
    - convert application properties to application yml

---
+++ src/main/java/local/joseramonterrerolopez/pruebacapitol/PruebacapitolApplication.java
@@ -0,0 +1,13 @@
+package local.joseramonterrerolopez.pruebacapitol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebacapitolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebacapitolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/local/joseramonterrerolopez/pruebacapitol/PruebacapitolApplicationTests.java
@@ -0,0 +1,13 @@
+package local.joseramonterrerolopez.pruebacapitol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebacapitolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
